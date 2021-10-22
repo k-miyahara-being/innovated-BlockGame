99df6cee143ebee149fe5b056e09aaad97e10f1d
machida-being <machida@beincorp.co.jp>
2021-10-22 16:50:47 +0900

    [リファクタリング] Ball.csの変数名の変更

--- BreakBlock/Ball.cs
+++ BreakBlock/Ball.cs
@@ -1,202 +1,172 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace BreakBlock
-{
-    class Ball
-    {
-        private PictureBox pictureBox; //描画するpictureBox
-        private Bitmap canvas;         //描画するキャンバス
-        private Brush brushColor;      //塗りつぶす色
-        private int previousX;         //以前の横位置(x座標)
-        private int previousY;         //以前の縦位置(y座標)
-        public int positionX;          //横位置(x座標)
-        public int positionY;          //縦位置(y座標)
-        public int directionX;         //移動方向(x座標)
-        public int directionY;         //移動方向(y座標)
-        public int radius;             //円の半径
-        public int pitch;              //移動の割合
-        public int score = 0;          //スコア
-        public int finish = 0;         //0のときまだプレイ中、1のときクリア、2のときゲームオーバー
-        private int hitNum;  　　　　　//跳ね返り回数の変数宣言
-        private int accel;　　　　　　 //加速度
+namespace BreakBlock {
+    class Ball {
+        private readonly PictureBox FPictureBox; //描画するpictureBox
+        private Bitmap FCanvas;         //描画するキャンバス
+        private Brush FBrushColor;      //塗りつぶす色
+        private int FPreviousX;         //以前の横位置(x座標)
+        private int FPreviousY;         //以前の縦位置(y座標)
+        public int FPositionX;          //横位置(x座標)
+        public int FPositionY;          //縦位置(y座標)
+        public int FDirectionX;         //移動方向(x座標)
+        public int FDirectionY;         //移動方向(y座標)
+        public int FRadius;             //円の半径
+        public int FPitch;              //移動の割合
+        public int FScore = 0;          //スコア
+        public int FFinish = 0;         //0のときまだプレイ中、1のときクリア、2のときゲームオーバー
+        private int FHitNum;  　　　　　//跳ね返り回数の変数宣言
+        private int FAccel;　　　　　　 //加速度
 
         //ボールコンストラクタ    
-        public Ball(PictureBox pb, Bitmap cv, Brush cl)
-        {
-            pictureBox = pb;            //描画するpictureBox
-            canvas = cv;                //描画するキャンバス
-            brushColor = cl;            //塗りつぶす色
+        public Ball(PictureBox vPb, Bitmap vCv, Brush vCl) {
+            FPictureBox = vPb;            //描画するpictureBox
+            FCanvas = vCv;                //描画するキャンバス
+            FBrushColor = vCl;            //塗りつぶす色
 
-            radius = 8;                 //円の半径の初期設定
-            pitch = radius / 2;         //移動の割合の初期設定(半径の半分)
+            FRadius = 8;                 //円の半径の初期設定
+            FPitch = FRadius / 2;         //移動の割合の初期設定(半径の半分)
 
             //ランダム発射
-            Random r = new Random();
-            int randomX = r.Next(0, 100) % 4;
-            if (randomX == 0)
-            {
-                directionX = -1;        //角度45°
+            Random wR = new Random();
+            int wRandomX = wR.Next(0, 100) % 4;
+            if (wRandomX == 0) {
+                FDirectionX = -1;        //角度45°
+            } else if (wRandomX == 1) {
+                FDirectionX = +1;        //角度(180-45)°
+            } else if (wRandomX == 2) {
+                FDirectionX = +2;        //角度22.5°
+                FPitch = FRadius / 3;
+            } else if (wRandomX == 3) {
+                FDirectionX = -2;        //角度(180-22.5)°
+                FPitch = FRadius / 3;
             }
-            else if(randomX == 1)
-            {
-                directionX = +1;        //角度(180-45)°
-            }
-            else if(randomX == 2)
-            {
-                directionX = +2;        //角度22.5°
-                pitch = radius / 3;     
-            }
-            else if(randomX == 3)
-            {
-                directionX = -2;        //角度(180-22.5)°
-                pitch = radius / 3;
-            }
-            directionY = -1;
+            FDirectionY = -1;
         }
 
         //指定した位置にボールを描く
-        public void PutCircle(int x, int y)
-        {
+        public void PutCircle(int wX, int wY) {
             //現在の位置を記憶
-            positionX = x;
-            positionY = y;
+            FPositionX = wX;
+            FPositionY = wY;
 
-            using (Graphics g = Graphics.FromImage(canvas))
-            {
+            using (Graphics g = Graphics.FromImage(FCanvas)) {
                 //弾をbrushColorで指定された色で描く
-                g.FillEllipse(brushColor, x - radius, y - radius, radius * 2, radius * 2);
+                g.FillEllipse(FBrushColor, wX - FRadius, wY - FRadius, FRadius * 2, FRadius * 2);
 
-                pictureBox.Image = canvas;
+                FPictureBox.Image = FCanvas;
             }
         }
 
         //指定した位置のボールを消す(黒で描く)
-        public void DeleteCircle()
-        {
+        public void DeleteCircle() {
 
             //初めて呼ばれて以前の値が無い時
-            if (previousX == 0)
-            {
-                previousX = positionX;
+            if (FPreviousX == 0) {
+                FPreviousX = FPositionX;
             }
-            if (previousY == 0)
-            {
-                previousY = positionY;
+            if (FPreviousY == 0) {
+                FPreviousY = FPositionY;
             }
 
-            using (Graphics g = Graphics.FromImage(canvas))
-            {
+            using (Graphics g = Graphics.FromImage(FCanvas)) {
                 //弾を黒で描く
-                g.FillEllipse(Brushes.Black, previousX - radius, previousY - radius, radius * 2, radius * 2);
+                g.FillEllipse(Brushes.Black, FPreviousX - FRadius, FPreviousY - FRadius, FRadius * 2, FRadius * 2);
 
-                pictureBox.Image = canvas;
+                FPictureBox.Image = FCanvas;
             }
 
         }
 
         //ボールを動かす
-        public void Move(List<Block> blocks)
-        {
+        public void Move(List<Block> vBlocks) {
 
             //以前の表示を削除
             DeleteCircle();
 
             //新しい移動先の計算
-            int x = positionX + (pitch + accel) * directionX;
-            int y = positionY + (pitch + accel) * directionY;
+            int wX = FPositionX + (FPitch + FAccel) * FDirectionX;
+            int wY = FPositionY + (FPitch + FAccel) * FDirectionY;
 
             //壁で跳ね返る補正
-            if (x >= canvas.Width - radius) //右端に来た場合の判定
+            if (wX >= FCanvas.Width - FRadius) //右端に来た場合の判定
             {
-                directionX = -1;
-                pitch = radius / 2;
+                FDirectionX = -1;
+                FPitch = FRadius / 2;
             }
-            if (x <= radius) //左端に来た場合の判定
+            if (wX <= FRadius) //左端に来た場合の判定
             {
-                directionX = +1;
-                pitch = radius / 2;
+                FDirectionX = +1;
+                FPitch = FRadius / 2;
             }
-            if (y <= radius) //上端に来た場合の判定
+            if (wY <= FRadius) //上端に来た場合の判定
             {
-                directionY = +1;
+                FDirectionY = +1;
             }
-            if (y >= canvas.Height)  //下端に来たときゲームオーバー画面に移る
+            if (wY >= FCanvas.Height)  //下端に来たときゲームオーバー画面に移る
             {
-                finish = 2;
+                FFinish = 2;
             }
 
             //ブロックに当たった時の跳ね返りとブロックを消す処理
-            for (int i = 0; i < blocks.Count; i++)
-            {
-                if ((y >= blocks[i].top - radius) && (y <= blocks[i].bottom + radius) && (x >= blocks[i].left - radius) && (x <= blocks[i].right + radius))
-                {
+            for (int wI = 0; wI < vBlocks.Count; wI++) {
+                if ((wY >= vBlocks[wI].top - FRadius) && (wY <= vBlocks[wI].bottom + FRadius) && (wX >= vBlocks[wI].left - FRadius) && (wX <= vBlocks[wI].right + FRadius)) {
                     Acceleration();
 
-                    if ((positionY < blocks[i].bottom + radius / 2) && (positionY > blocks[i].top - radius / 2))
-                    {
-                        directionX *= -1; //左右からきた
+                    if ((FPositionY < vBlocks[wI].bottom + FRadius / 2) && (FPositionY > vBlocks[wI].top - FRadius / 2)) {
+                        FDirectionX *= -1; //左右からきた
+                    } else if ((FPositionX > vBlocks[wI].left - FRadius / 2) && (FPositionX < vBlocks[wI].right + FRadius / 2)) {
+                        FDirectionY *= -1;  //上下からきた
+                    } else  //斜めからきた
+                      {
+                        FDirectionX *= -1;
+                        FDirectionY *= -1;
                     }
-                    else if ((positionX > blocks[i].left - radius / 2) && (positionX < blocks[i].right + radius / 2))
-                    {
-                        directionY *= -1;  //上下からきた
-                    }
-                    else  //斜めからきた
-                    {
-                        directionX *= -1;
-                        directionY *= -1;
-                    }
-                    blocks[i].DeleteBlock();
-                    blocks.RemoveAt(i);
-                    score += 10;
+                    vBlocks[wI].DeleteBlock();
+                    vBlocks.RemoveAt(wI);
+                    FScore += 10;
                     break;
                 }
             }
 
             //バーに衝突すると跳ね返る
-            if (y >= 350 - radius && y <= 350)
-            {
-                if (x >= Bar.barpositionX - 10 && x <= Bar.barpositionX + 100)
-                {
-                    directionY = -1;
-                    if (x < Bar.barpositionX + 30)　　//バーの左側
+            if (wY >= 350 - FRadius && wY <= 350) {
+                if (wX >= Bar.barpositionX - 10 && wX <= Bar.barpositionX + 100) {
+                    FDirectionY = -1;
+                    if (wX < Bar.barpositionX + 30)　　//バーの左側
                     {
-                        directionX = -2;
-                        pitch = 2;
+                        FDirectionX = -2;
+                        FPitch = 2;
                     }
-                    if (x > Bar.barpositionX + 60)　　//バーの右側
+                    if (wX > Bar.barpositionX + 60)　　//バーの右側
                     {
-                        directionX = +2;
-                        pitch = 2;
+                        FDirectionX = +2;
+                        FPitch = 2;
                     }
                 }
             }
 
             //跳ね返り補正を反映した値で新しい位置を計算
-            positionX = x + directionX;
-            positionY = y + directionY;
+            FPositionX = wX + FDirectionX;
+            FPositionY = wY + FDirectionY;
 
             //新しい位置に描画
-            PutCircle(positionX, positionY);
+            PutCircle(FPositionX, FPositionY);
 
             //新しい位置を以前の値として記憶
-            previousX = positionX;
-            previousY = positionY;
+            FPreviousX = FPositionX;
+            FPreviousY = FPositionY;
         }
 
         //加速処理
-        private void Acceleration()
-        {
-            hitNum += 1;
-            if (hitNum == 4)
-            {
-                accel += 1;
-                hitNum = 0;
+        private void Acceleration() {
+            FHitNum += 1;
+            if (FHitNum == 4) {
+                FAccel += 1;
+                FHitNum = 0;
             }
         }
     }
