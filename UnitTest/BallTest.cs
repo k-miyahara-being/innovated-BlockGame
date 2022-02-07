51206a10dbe0e15e550bd10b556bd92bf6897804
BEING\machida <machida@beingcorp.co.jp>
2022-02-07 10:20:01 +0900

    Ballクラスのテストコードの実装完了

--- UnitTest/BallTest.cs
+++ UnitTest/BallTest.cs
@@ -6,45 +6,14 @@ using NUnit.Framework;
 namespace UnitTest {
     [TestFixture]
     class BallTest {
-
-        [Test]
-        public void Move_弾が元の位置から移動している() {
-            var wBall = new Ball(UnitTestDefine.C_BallPosX, UnitTestDefine.C_BallPosY); 
-            Vector wPoistionBeforeMove = wBall.Position;
-            wBall.Move();
-            Assert.AreNotEqual(wPoistionBeforeMove, wBall.Position);
-        }
-        //TODO:壁に当たった時の弾の位置を加える
-        [TestCase(Orientation.Horizontal)]
-        public void Reverse_弾が横の壁に当たった時に弾のxが反転する(Orientation vOrientation) {
-            var wBall = new Ball(UnitTestDefine.C_BallPosX, UnitTestDefine.C_BallPosY);
-            Ball wBallBeforeReverse = wBall;
-
-            Vector wSpeed = wBallBeforeReverse.Speed;
-            wSpeed.X *= -1;
-            wBallBeforeReverse.Speed += wSpeed;
+        [TestCase(Orientation.Horizontal, -1, 1, TestName = "水平方向の時はxが反転すること")]
+        [TestCase(Orientation.Vertical, 1, -1, TestName = "垂直方向の時はyが反転すること")]
+        public void Speedが反転すること(Orientation vOrientation, int vResultX, int vResultY) {
+            var wBall = new Ball(0, 0);
+            wBall.Speed = new Vector(1, 1);
             wBall.Reverse(vOrientation);
-            Assert.AreEqual(wBallBeforeReverse.Speed, wBall.Speed);
+            Assert.AreEqual(vResultX, wBall.Speed.X);
+            Assert.AreEqual(vResultY, wBall.Speed.Y);
         }
-        [TestCase(Orientation.Vertical)]
-        public void Reverse_弾が上の壁にあたったときに弾のyが反転する(Orientation vOrientation) {
-            var wBall = new Ball(UnitTestDefine.C_BallPosX, UnitTestDefine.C_BallPosY);
-            Ball wBallBeforeReverse = wBall;
-            Vector wSpeed = wBallBeforeReverse.Speed;
-            wSpeed.Y *= -1;
-            wBallBeforeReverse.Speed += wSpeed;
-            wBall.Reverse(vOrientation);
-            Assert.AreEqual(wBallBeforeReverse.Speed, wBall.Speed);
-        }
-        //TODO:ChangeDirection()のテスト
-
-        [Test]
-        public void Accelerate_Speedに加速度が加算されている() {
-            var wBall = new Ball(UnitTestDefine.C_BallPosX, UnitTestDefine.C_BallPosY);
-            Vector wSpeedBeforeAdd = wBall.Position;
-            wBall.Accelerate();
-            Assert.AreNotEqual(wSpeedBeforeAdd, wBall.Speed);
-        }
-
     }
 }
