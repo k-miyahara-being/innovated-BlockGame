4c2fd41da1a0e8688ed7237efb69c6ae90efe78b
BEING\machida <machida@beingcorp.co.jp>
2022-02-08 16:36:18 +0900

    Ballクラス・Utilsクラスの基本的なテストコード実装完了

--- UnitTest/BallTest.cs
+++ UnitTest/BallTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Forms;
+using System.Drawing;
 using BreakBlock;
 using NUnit.Framework;
 
@@ -9,42 +10,48 @@ namespace UnitTest {
     class BallTest {
         //Reverse()のテスト
         [Test]
-        public void 水平方向の時にSpeedのxが反転すること() {
-            var wBall = new Ball(0, 0, 0, new Vector(1, 1));
-            Ball wBallBeforeReverse = wBall;
+        public void Speedのxとyが反転すること() {
+            var wBall = new Ball(0, 0, 0, new Vector(0, 0));
+            wBall.Speed = new Vector(1, 1);
+            var wOldBallSpeed = new Vector(1, 1);
             wBall.Reverse(Orientation.Horizontal);
-            Assert.IsFalse(Math.Sign(wBallBeforeReverse.Speed.X) != Math.Sign(wBall.Speed.X));
-        }
-        [Test]
-        public void 垂直方向の時にSpeedのyが反転すること() {
-            var wBall = new Ball(0, 0, 0, new Vector(1, 1));
-            Ball wBallBeforeReverse = wBall;
             wBall.Reverse(Orientation.Vertical);
-            Assert.IsFalse(Math.Sign(wBallBeforeReverse.Speed.Y) != Math.Sign(wBall.Speed.Y));
+            Assert.IsTrue(Math.Sign(wOldBallSpeed.X) != Math.Sign(wBall.Speed.X));
+            Assert.IsTrue(Math.Sign(wOldBallSpeed.Y) != Math.Sign(wBall.Speed.Y));
         }
+
         //VsWall()のテスト
-        [Test]
-        public void 弾が左の壁に当たった時にLeftが返ること() {
-            var wBall = new Ball(1, 1, 1, new Vector(1, 1));
-            HitPointWall? wResult = wBall.VsWall(10, 10);
-            Assert.AreEqual(HitPointWall.Left, wResult);
+        [TestCase(5, 10, 5, HitPointWall.Left, TestName ="左に当たった時にLeftが返ること")]
+        [TestCase(15, 10, 5, HitPointWall.Right, TestName = "右に当たった時にRightが返ること")]
+        [TestCase(10, 5, 5, HitPointWall.Top, TestName = "上に当たった時にTopが返ること")]
+        [TestCase(10, 15, 5, HitPointWall.Bottom, TestName = "下に当たった時にBottomが返ること")]
+        public void 弾が壁に当たった時にその場所が返ること(int vBallPosX, int vBallPosY, int vBallRadius, HitPointWall vExpected) {
+            var wBall = new Ball(vBallPosX, vBallPosY, vBallRadius, new Vector(0, 0));
+            HitPointWall? wResult = wBall.VsWall(20, 20);
+            Assert.AreEqual(vExpected, wResult);
         }
-        [Test]
-        public void 弾が右の壁に当たった時にRightが返ること() {
-            var wBall = new Ball(6, 5, 5, new Vector(1, 1));
-            HitPointWall? wResult = wBall.VsWall(10, 10);
-            Assert.AreEqual(HitPointWall.Right, wResult);
+
+        //VsBar()のテスト
+        [TestCase(5, HitPointBar.First, TestName ="Firstが返ること")]
+        [TestCase(15, HitPointBar.Second, TestName = "Secondが返ること")]
+        [TestCase(25, HitPointBar.Third, TestName = "Thirdが返ること")]
+        [TestCase(35, HitPointBar.Fourth, TestName = "Fourthが返ること")]
+        [TestCase(45, HitPointBar.Fifth, TestName = "Fifthが返ること")]
+        public void barに当たった部分が返ること(int vBallPosX, HitPointBar vExpected) {
+            var wBall = new Ball(vBallPosX, 10, 1, new Vector(0, 0));
+            HitPointBar? wResult = wBall.VsBar(new Bar(0, 10, 50, 10, 10));
+            Assert.AreEqual(vExpected, wResult);
         }
-        [Test]
-        public void 弾が上の壁に当たった時にTopが返ること() {
-            var wBall = new Ball(2, 1, 1, new Vector(1, 1));
-            HitPointWall? wResult = wBall.VsWall(10, 10);
-            Assert.AreEqual(HitPointWall.Top, wResult);
-        }
-        public void 弾が下の壁に当たった時にBottomが返ること() {
-            var wBall = new Ball(5, 5, 5, new Vector(1, 1));
-            HitPointWall? wResult = wBall.VsWall(10, 10);
-            Assert.AreEqual(HitPointWall.Bottom, wResult);
+        //VsBlock()のテスト
+        [TestCase(20, 5, 6, 0, 1, Orientation.Vertical, TestName ="ブロックの上に当たった時にVerticalが返ること")]
+        [TestCase(20, 25, 6, 0, -1, Orientation.Vertical, TestName = "ブロックの下に当たった時にVerticalが返ること")]
+        [TestCase(5, 15, 6, 1, 0, Orientation.Horizontal, TestName = "ブロックの左に当たった時にHorizontalが返ること")]
+        [TestCase(35, 15, 6, -1, 0, Orientation.Horizontal, TestName = "ブロックの右に当たった時にHorizontalが返ること")]
+        public void 弾がブロックに当たった時に弾の方向が返ること(int vBallPosX, int vBallPosY, int vBallRadius, int vBallSpeedX, int vBallSpeedY, Orientation vExpected) {
+            var wBall = new Ball(vBallPosX, vBallPosY, vBallRadius, new Vector(0, 0));
+            wBall.Speed = new Vector(vBallSpeedX, vBallSpeedY);
+            Orientation? wResult = wBall.VsBlock(new Rectangle(10 ,10, 20, 10));
+            Assert.AreEqual(vExpected, wResult);
         }
     }
 }
---
+++ UnitTest/UtilsTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using BreakBlock;
+using NUnit.Framework;
+
+namespace UnitTest {
+    class UtilsTest {
+        [Test]
+        public void 円と直線が当たるとTrueを返しそれ以外はfalseが返ること() {
+            Assert.IsTrue(Utils.IsCircleHitLine(new Vector(0, 5), new Vector(5, 5), new Vector(3, 3), 3));
+            Assert.IsFalse(Utils.IsCircleHitLine(new Vector(0, 5), new Vector(5, 5), new Vector(10, 10), 3));
+        }
+    }
+}
