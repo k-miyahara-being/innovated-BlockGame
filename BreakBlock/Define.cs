42361c6b62dcaecf273b271af1486520c040374f
k-miyahara-being <k-miyahara@beingcorp.co.jp>
2022-02-15 12:04:53 +0900

    コメントの追加、static readonlyをconstに置換、不要なusingを削除

--- BreakBlock/Define.cs
+++ BreakBlock/Define.cs
@@ -3,30 +3,30 @@
 namespace BreakBlock {
     public static class Define {
         //弾関連
-        public static readonly int C_BallNum = 3;
-        public static readonly double C_Acceleration = 1.05;
-        public static readonly int C_LaunchAngleMin = 20;
-        public static readonly int C_LaunchAngleMax = 71;
-        public static readonly int C_LaunchVelocity = -3;
+        public const int C_BallNum = 3;
+        public const double C_Acceleration = 1.05;
+        public const int C_LaunchAngleMin = 20;
+        public const int C_LaunchAngleMax = 71;
+        public const int C_LaunchVelocity = -3;
 
-        public static readonly int C_SmallBallX = 20;
-        public static readonly int C_SmallBallY = 383;
-        public static readonly int C_SmallBallRadius = 6;
+        public const int C_SmallBallX = 20;
+        public const int C_SmallBallY = 383;
+        public const int C_SmallBallRadius = 6;
 
         //バー関連
-        public static readonly int C_BarPositionY = 350;
-        public static readonly int C_BarMoveDistance = 20;
+        public const int C_BarPositionY = 350;
+        public const int C_BarMoveDistance = 20;
 
         //ブロック関連
-        public static readonly int C_BlockDrawingAreaWidth = 324;
-        public static readonly int C_BlockDrawingAreaHeight = 100;
-        public static readonly int C_BlockFirstPositionX = 10;
-        public static readonly int C_BlockFirstPositionY = 20;
+        public const int C_BlockDrawingAreaWidth = 324;
+        public const int C_BlockDrawingAreaHeight = 100;
+        public const int C_BlockFirstPositionX = 10;
+        public const int C_BlockFirstPositionY = 20;
 
         //スコア関連
-        public static readonly int C_ScoreAddition = 10;
-        public static readonly int C_ScoreSubtraction = 20;
-        public static readonly int C_ScoreBonus = 30;
+        public const int C_ScoreAddition = 10;
+        public const int C_ScoreSubtraction = 20;
+        public const int C_ScoreBonus = 30;
 
         //アニメーションの色
         public static readonly Brush[] GameOverColors = new Brush[] {
--- BreakBlock/DifficultyItem.cs
+++ BreakBlock/DifficultyItem.cs
@@ -1,19 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BreakBlock {
+﻿namespace BreakBlock {
+    /// <summary>
+    /// コンボボックスのItemクラス
+    /// </summary>
     public class DifficultyItem {
+        /// <summary>
+        /// コンボボックスに表示する文字列
+        /// </summary>
         public string Text { get; set; }
+        /// <summary>
+        /// プログラムで扱う文字列
+        /// </summary>
         public string Value { get; set; }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vText"></param>
+        /// <param name="vValue"></param>
         public DifficultyItem(string vText, string vValue) {
             this.Text = vText;
             this.Value = vValue;
         }
 
+        /// <summary>
+        /// コンボボックスに文字列を表示する
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() {
             return this.Text;
         }
